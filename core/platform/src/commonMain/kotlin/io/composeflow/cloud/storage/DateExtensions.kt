9fefb066d7ff37a8adaeb3be34ab8b0cf3b58edd
Masashi Katsumata <wf9a5m75@gmail.com>
2025-05-23 18:41:42 +0900

    - Fix: asDateString() uses `TimeZone.currentSystemDefault()` inside. Provide the timezone as parameter. (#220)

--- core/platform/src/commonMain/kotlin/io/composeflow/cloud/storage/DateExtensions.kt
+++ core/platform/src/commonMain/kotlin/io/composeflow/cloud/storage/DateExtensions.kt
@@ -8,8 +8,8 @@ import java.time.OffsetDateTime
 fun OffsetDateTime.toKotlinxInstant(): Instant =
     Instant.fromEpochSeconds(this.toEpochSecond(), this.nano.toLong())
 
-fun Instant.asDateString(): String {
-    val date = toLocalDateTime(TimeZone.currentSystemDefault()).date
+fun Instant.asDateString(timeZone: TimeZone? = null): String {
+    val date = toLocalDateTime(timeZone ?: TimeZone.currentSystemDefault()).date
     return "${date.year}-${
         date.monthNumber.toString().padStart(2, '0')
     }-${date.dayOfMonth.toString().padStart(2, '0')}"
